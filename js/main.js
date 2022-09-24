0ae720024187c7e6b88806e706dac3a604436f0e
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-09-24 20:07:00 +0500

    Add responsibility into site-header and hero (to 650 px)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const elLoader = document.querySelector(".lds-hourglass-wrapper");
+const elLoader = document.querySelector(".lds-hourglass-wrapper"),
+elToggler = document.querySelector(".js-toggler"),
+elSitenav = document.querySelector(".js-sitenav");
 
+// LOADER
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function (){
     elLoader.classList.add("lds-hourglass-wrapper--none")
   }, 800);
 });
+
+
+// TOGGLER
+if (elToggler) {
+  elToggler.addEventListener("click", function () {
+    elToggler.classList.toggle("toggler--open");
+    elSitenav.classList.toggle("site-header__sitenav--open");
+  });
+}
