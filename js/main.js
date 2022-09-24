743cd642cb7b8f408aa4bda6dc4c4bf5197c0385
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-09-24 22:17:18 +0500

    Fix overflow mistake and add responsibility ( 400px)

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // TOGGLER
 if (elToggler) {
   elToggler.addEventListener("click", function () {
+    document.body.classList.toggle("body--overflow")
     elToggler.classList.toggle("toggler--open");
     elSitenav.classList.toggle("site-header__sitenav--open");
   });
